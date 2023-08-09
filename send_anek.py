60db5dee29da7184841878aff38ef34e838264c0
German Berezhko <gerralizza@gmail.com>
2023-08-09 21:51:53 +0100

    Fix scripts

--- send_anek.py
+++ send_anek.py
@@ -3,25 +3,39 @@ import os
 import asyncio
 import datetime
 
+from telegram.error import TelegramError
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+script_file_name = os.path.basename(__file__)
+
 
 async def send_anek():
     token = os.environ.get('TG_BOT_TOKEN')
     channel_id = os.environ.get('TG_NAILS_CHANNEL_ID')
+    anek_file_path = os.path.join(script_directory, "anek_nails.txt")
 
-    with open("anek_nails.txt", "r") as f:
+    with open(anek_file_path, "r") as f:
         text = f.read()
         bot = Bot(token=token)
-        await bot.send_message(chat_id=channel_id, text=text)
+        try:
+            log("Sending a message...")
+            await bot.send_message(chat_id=channel_id, text=text)
+            log("Message sent!")
+        except TelegramError as e:
+            log(e)
+        except Exception as e:
+            log(f"Error: {e.__str__()}")
 
 
 def log(msg):
-    with open("logs.txt", "a") as f:
-        script_file_name = os.path.basename(__file__)
+    logs_file_path = os.path.join(script_directory, "logs.txt")
+    with open(logs_file_path, "a") as f:
         date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        f.write(f"[{script_file_name}] {date} - {msg}\n")
+        msg_to_log = f"[{script_file_name}] {date} - {msg}"
+        f.write(f"{msg_to_log}\n")
+        print(msg_to_log)
 
 
 if __name__ == '__main__':
-    log("I'm HERE")
-
+    log("Starting a script...")
     asyncio.run(send_anek())
