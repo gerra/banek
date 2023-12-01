cb7c673d79d9c470ce227bba37719403341e49c1
German Berezhko <gerralizza@gmail.com>
2023-12-01 21:27:51 +0000

    Move the whole logic to python script

--- send_anek.py
+++ send_anek.py
@@ -1,8 +1,10 @@
-from telegram import Bot
-import os
 import asyncio
 import datetime
+import os
+import random
+import subprocess
 
+from telegram import Bot
 from telegram.error import TelegramError
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -28,6 +30,25 @@ async def send_anek():
             log(f"Error: {e.__str__()}")
 
 
+def schedule_next():
+    hour = random.randint(15, 19)
+    minute = random.randint(0, 59)
+    schedule_next_specific(hour, minute, True)
+
+
+def schedule_next_specific(hour, minute, tomorrow):
+    formatted_time = "{:02d}:{:02d}".format(hour, minute)
+    if tomorrow:
+        formatted_time += " tomorrow"
+
+    command_to_run = f"echo \"python3 $HOME/Projects/banek/send_anek.py\" | at \"{formatted_time}\""
+
+    log(f"Scheduling new send by command [{command_to_run}]")
+
+    result = subprocess.run(command_to_run, shell=True, check=True, text=True, stderr=subprocess.PIPE)
+    log(f"Result: {result.stderr.rstrip()}")
+
+
 def log(msg):
     logs_file_path = os.path.join(script_directory, "logs.txt")
     with open(logs_file_path, "a") as f:
@@ -40,3 +61,11 @@ def log(msg):
 if __name__ == '__main__':
     log("Starting a script...")
     asyncio.run(send_anek())
+
+    schedule_next()
+    # schedule_next_specific(21, 25, False)
+
+# atq -- show scheduled jobs
+# atrm X -- remove job X
+# at -c X -- show info about job X
+# atrm $(atq | cut -f1) -- remove all jobs
